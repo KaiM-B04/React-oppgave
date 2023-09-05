8eaebadecc16ec27d3fee30411496fe59d886fd0
kai <kaim.kodehode@gmail.com>
2023-09-05 13:31:36 +0200

    litt omoragnisering og masse styling

--- src/Components/ClassCard.jsx
+++ src/Components/ClassCard.jsx
@@ -2,19 +2,24 @@ const ClassCard = ({ className, description, subclasses, features }) => {
   return (
     <div className="class-card">
       <h2>{className}</h2>
+      {/* <img src={image} /> */}
       <p>{description}</p>
-      <ul>
-        <h3>Subclasses</h3>
-        {subclasses.map((subclasses, index) => (
-          <li key={index}>{subclasses}</li>
-        ))}
-      </ul>
-      <ul>
-        <h3>Unique Features</h3>
-        {features.map((features, index) => (
-          <li key={index}>{features}</li>
-        ))}
-      </ul>
+      <div className="subClass">
+        <ul>
+          <h3>Subclasses</h3>
+          {subclasses.map((subclasses, index) => (
+            <li key={index}>{subclasses}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="features">
+        <ul>
+          <h3>Unique Features</h3>
+          {features.map((features, index) => (
+            <li key={index}>{features}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/JavaScript/classes.jsx
+++ src/JavaScript/classes.jsx
@@ -1,21 +1,26 @@
 const classesData = [
   {
+    id: 1,
     name: "Barbarian",
     description:
       "The strong embrace the wild that hides inside - keen instincts, primal physicality, and most of all, an unbridled, unquenchable rage.",
     subclasses: ["Berserker", "Wildheart", "Wild Magic"],
     features: ["Unarmoured Defense", "Rage"],
+    image: "/images/Barbarian.png",
   },
 
   {
+    id: 2,
     name: "Bard",
     description:
       "You know music is more than a fancy - it is power. Through study and adventure, you have mastered song, speech, and the magic within.",
     subclasses: ["College of Lore", "College of Valour", "College of Swords"],
     features: ["Bardic Inspiration"],
+    image: "/images/Bard.png",
   },
 
   {
+    id: 3,
     name: "Cleric",
     description:
       "Clerics are representatives of the gods they worship, wielding potent divine magic for good or ill.",
@@ -29,9 +34,11 @@ const classesData = [
       "War Domain",
     ],
     features: ["Domain Spells"],
+    image: "/React-oppgave/public/images/Cleric.png",
   },
 
   {
+    id: 4,
     name: "Druid",
     description:
       "Druids channel the elemental forces of nature and share a deep kinship with animals. Mastery of Wild Shape allows them to transform into beasts from all over the Realms.",
@@ -41,17 +48,21 @@ const classesData = [
       "Circle of the Spores",
     ],
     features: ["Wildshape"],
+    image: "/images/Druid.png",
   },
 
   {
+    id: 5,
     name: "Fighter",
     description:
       "Fighters have mastered the art of combat, wielding weapons with unmatched skill and wearing armour like a second skin.",
     subclasses: ["Battle Master", "Eldritch Knight", "Champion"],
     features: ["Second Wind (Action)", "Fighting Style"],
+    image: "/images/Fighter.png",
   },
 
   {
+    id: 6,
     name: "Monk",
     description:
       "Channel your cosmic enlightenment by deftly dodging and efficiently disassembling your foes through stunning strikes and a whirlwind of martial art attacks.",
@@ -68,9 +79,11 @@ const classesData = [
       "Martial Arts: Deft Strikes",
       "Martial Arts: Bonus Unarmed Strike",
     ],
+    image: "/images/Monk.png",
   },
 
   {
+    id: 7,
     name: "Paladin",
     description:
       "Fuelled by the Oaths you swore to uphold justice and righteousness, you are a beacon of hope in dark times.",
@@ -84,38 +97,47 @@ const classesData = [
   },
 
   {
+    id: 8,
     name: "Ranger",
     description:
       "Rangers are unrivalled scouts and trackers, honing a deep connection with nature in order to hunt their favoured prey.",
     subclasses: ["Beast Master", "Hunter", "Gloom Stalker"],
     features: ["Favoured Enemy", "Natural Explorer"],
+    image: "/images/Ranger.png",
   },
 
   {
+    id: 9,
     name: "Rogue",
     description:
       "With stealth, skill and uncanny reflexes, a rogue's versatility lets them get the upper hand in almost any situation.",
     subclasses: ["Thief", "Arcane Trickster", "Assassin"],
     features: ["Sneak Attack (Melee)", "Sneak Attack (Ranged)"],
+    image: "/images/Rogue.png",
   },
 
   {
+    id: 10,
     name: "Sorcerer",
     description:
       "Sorcerers are natural spellcasters, drawing on inherent magic from a gift or bloodline.",
     subclasses: ["Draconic Bloodline", "Wild Magic", "Storm Sorcery"],
     features: ["Sorccery Points"],
+    image: "/images/Sorcerer.png",
   },
 
   {
+    id: 11,
     name: "Warlock",
     description:
       "Bound by a pact to an all-powerful patron, warlocks trade their loyalty for supernatural abilities and unique magic.",
     subclasses: ["The Fiend", "The Great Old One", "Archfey"],
     features: ["Eldritch Blast"],
+    image: "/images/Warlock.png",
   },
 
   {
+    id: 12,
     name: "Wizard",
     description:
       "Wizards master the arcane by specialising in individual schools of magic, combining ancient spells with modern research.",
@@ -130,6 +152,7 @@ const classesData = [
       "Transmutation School",
     ],
     features: ["Arcane Recovery"],
+    image: "./images/Wizard.png",
   },
 ];
 
